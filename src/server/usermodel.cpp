325af8b0f0866403c118bfd0093bbe48062e520d
klchen <klchen_auto@163.com>
2024-02-28 01:01:37 +0800

    reg request test finish

--- src/server/usermodel.cpp
+++ src/server/usermodel.cpp
@@ -10,7 +10,7 @@ using namespace std;
 bool UserModel::insert(User& user){
     // 1. 组装SQL
     char sql[1024] = {0};
-    sprintf(sql,"insert int User(name, password, state) values('%s', '%s', '%s')",
+    sprintf(sql,"insert into User(name, password, state) values('%s', '%s', '%s')",
         user.getName().c_str(), user.getPwd().c_str(), user.getState().c_str());
     
     // 2. 定义Mysql对象
